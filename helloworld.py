0de4004c94e84cd6e35644ff375cca4b1a4fa081
nfiner <nfiner@umich.edu>
2019-09-09 13:34:13 -0400

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from random import randint
+
+print("Noah")
+
+num1 = randint(0, 100)
+num2 = randint(0, 100)
+
+print(num1)
+print(num2)
+print("Sum = " + str(num1 + num2))
+print("Average = " + str(float(num1 + num2)/2))
